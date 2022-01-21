25dd03f1cb3ca37c3b0a68a37e883153a3fe4f0e
Caswell Orr <caswell.orr@gmail.com>
2022-01-20 18:45:51 -0600

    indexedDb methods completed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,9 +1,13 @@
+
+
 import { openDB } from 'idb';
 
+// =========== INITIALIZE INDEXED DATABASE ============
+
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jateDb', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jateDb')) {
         console.log('jate database already exists');
         return;
       }
@@ -12,10 +16,60 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  // ========== ADD CONTENT TO DB ==============
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+
+  try {
+
+    console.log('Added to jate database');
+
+    const jateDb = await openDB('jateDb', 1);
+
+    const tx = jateDb.transaction('jateDb', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.add({content});
+
+    const result = await request;
+
+    console.log('🚀 - data saved to the database', result);
+
+  } catch (error) {
+    
+    console.error('putDb not implemented');
+
+  };
+};
+
+// ========== RETRIEVE ALL CONTENT FROM DB ============
+
+export const getDb = async () => {
+
+  try {
+
+    console.log('Retrieve all data from the database');
+
+    const jateDb = await openDB('jateDb', 1);
+
+    const tx = jateDb.transaction('jateDb', 'readonly');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.getAll();
+
+    const result = await request;
+
+    console.log('result.value', result);
+
+    return result;
+
+  } catch (error) {
+    
+    console.error('getDb not implemented');
+
+  };
+};
 
 initdb();
