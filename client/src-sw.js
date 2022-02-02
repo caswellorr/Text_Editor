6e46639366fc377693296e5f9f3a411d8b3598bb
Caswell Orr <caswell.orr@gmail.com>
2022-02-02 15:59:32 -0600

    implemented install handlers

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,6 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
 
 // Set up asset cache
 registerRoute(
